e82241b37c86516c6ed6a9bd9fab6f5cf017a94b
Oleksandr Revva <revvao@gmail.com>
2022-12-02 09:46:31 +0200

    tasks 1 and 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,4 @@
+const listElement = document.querySelector('#ingredients');
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +7,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const fragment = document.createDocumentFragment();
+const liElements = ingredients.forEach(ingredient => {
+  const liElement = document.createElement('li');
+  liElement.classList.add('item');
+  liElement.textContent = ingredient;
+  fragment.appendChild(liElement);
+  listElement.appendChild(liElement);
+});
+
+listElement.appendChild(fragment);
